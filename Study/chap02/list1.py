51a70b41323de7cdf57292d88ea58bcaac8b0c4c
DESKTOP-8MU057U\bbsr0 <bbsr0819@gmail.com>
2025-01-13 23:21:35 +0900

    25.01.13 commit

---
+++ Study/chap02/list1.py
@@ -0,0 +1,6 @@
+x = ['John', 'George', 'Paul', 'Ringo']
+
+for i in range(len(x)):
+    print(f'x[{i}] = {x[i]}')
+
+    
---
+++ Study/chap02/list3.py
@@ -0,0 +1,6 @@
+x = ['John', 'George', 'Paul', 'Ringo']
+
+for i, name in enumerate(x, 1):
+    print(f'{i}번째 = {name}')
+
+    
