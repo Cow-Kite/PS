c41c2fc9f3ebaada103c649d27e8fe0e236c86ea
DESKTOP-8MU057U\bbsr0 <bbsr0819@gmail.com>
2025-01-22 15:32:24 +0900

    25.01.22 commit

---
+++ Study/03.Search/chained_hash.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+from typing import Any, Type
+import hashlib
+
+class Node:
+    """해시를 구성하는 노드"""
+    def __init__(self, key: Any, value: Any, next: Node) -> None:
+        self.key = key
+        self.value = value
+        self.next = next
+
+class ChainedHash:
+    """체인법으로 해시 클래스 구현"""
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity
+        self.table = [None] * self.capacity
+
+    def hash_value(self, key: Any) -> int:
+        """해시값을 구함"""
+        if isinstance(key, int):
+            return key % self.capacity
+        return(int(hashlib.sha256(str(key).encode()).hexdigest(), 16) % self.capacity)
+    
+    def search(self, key: Any) -> Any:
+        """키가 key인 원소를 검색하여 값을 반환"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return p.value
+            p = p.next
+
+        return None
+    
+    def add(self, key: Any, value: Any) -> bool:
+        """키가 key이고 값이 value인 원소를 추가"""
+        hash = self.hash_value(key) # 추가하는 key의 해시값
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return False
+            p = p.next
+
+        temp = Node(key, value, self.table[hash])
+        self.table[hash] = temp
+        return True
+    
+    """update 예정..."""
