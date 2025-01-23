84467f846e35674db94133587366486857d9398c
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2025-01-24 03:16:34 +0900

    [level 2] Title: 기능개발, Time: 0.34 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,24 +1,20 @@
+from queue import Queue
 def solution(progresses, speeds):
     answer = []
-    ptr = 0
+    days = Queue()
     
-    while True:
-        cnt = 0
-        if ptr >= len(progresses):
-            break
-            
-        for i in range(ptr, len(progresses)):
-            progresses[i] += speeds[i]
+    for progress, speed in zip(progresses, speeds):
+        remain = 100 - progress
+        day = (remain // speed) + (1 if remain % speed != 0 else 0)
+        days.put(day)
         
-        for j in range(ptr, len(progresses)):
-            if progresses[j] >= 100: 
-                cnt += 1
-            else:
-                break     
-            
-        ptr += cnt
-                
-        if cnt > 0:
-            answer.append(cnt)
+    while not days.empty():
+        cnt = 1
+        first = days.get()
+        while not days.empty() and days.queue[0] <= first:
+            days.get()
+            cnt += 1
+        
+        answer.append(cnt)
             
     return answer
