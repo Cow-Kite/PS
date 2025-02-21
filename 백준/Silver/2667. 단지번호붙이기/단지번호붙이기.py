f1d2f9aa0dbcad76f8ceb29ad36002542e8e8f31
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2025-02-21 23:21:04 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,13 +2,7 @@ from collections import deque
 
 N = int(input())
 
-house = [[0] * (N) for _ in range(N)]
-
-for i in range(N):
-    line = input()
-    for j in range(N):
-        house[i][j] = int(line[j])
-
+house = [list(map(int, input().strip())) for _ in range(N)]
 queue = deque()
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
