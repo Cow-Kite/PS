bef9803e6d9222ecc8068c45462e5e64828d9332
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2025-04-08 00:57:34 +0900

    [Gold III] Title: 감시, Time: 860 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,16 +1,15 @@
-di = [-1,0,1,0]
-dj = [0,1,0,-1] # 상우하좌
-cctv = [[], [1], [1,3], [0,1], [0,1,3], [0,1,2,3]]
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1] # 90도씩 회전하기 때문에 상우하좌
+cctv = [[], [1], [1, 3], [0, 1], [0, 1, 3], [0, 1, 2, 3]] # cctv 종류에 따른 보는 방향
 
-def cal(tlst):
-    v = [[0]*(M+2) for _ in range(N+2)]
-    #  모든 cctv에 대해서 처리
+def cal(tlst): # 사각지대 넓이 계산 하는 함수
+    v = [[0]*(M+2) for _ in range(N+2)] # cctv로 봤는지 기록하는 변수
     for i in range(CNT):
-        si, sj = lst[i]
-        rot = tlst[i]
-        type = arr[si][sj]
+        si, sj = lst[i] # cctv 좌표
+        rot = tlst[i] # i번 cctv가 회전하는 정도 90도인지 180도인지 등
+        type = arr[si][sj] # cctv 종류 1~5까지
 
-        # type에 따라 모든 방향을 뻗어가면서 v[] 1표시
+        # type에 따라 모든 방향을 뻗어나가면서 v배열에 1로 표시를 할거야
         for dr in cctv[type]:
             dr = (dr+rot)%4
             ci, cj = si, sj
@@ -20,7 +19,7 @@ def cal(tlst):
                     break
                 v[ci][cj] = 1
 
-    # 사각지대(0이고, 미방문) 개수 카운트
+        # 사각지대 넓이 카운트
     cnt = 0
     for i in range(1, N+1):
         for j in range(1, M+1):
@@ -30,16 +29,17 @@ def cal(tlst):
 
 def dfs(n, tlst):
     global ans
-    if n == CNT: # 모든 cctv 방향 정하기 원료
+    if n == CNT: # 모든 cctv 방향 정하기 완료
         ans = min(ans, cal(tlst))
         return
-    dfs(n+1, tlst+[0]) #   0도 회전
-    dfs(n+1, tlst+[1]) #  90도 회전
-    dfs(n+1, tlst+[2]) # 180도 회전
-    dfs(n+1, tlst+[3]) # 270도 회전
+
+    dfs(n+1, tlst+[0])
+    dfs(n+1, tlst+[1])
+    dfs(n+1, tlst+[2])
+    dfs(n+1, tlst+[3])
 
 N, M = map(int, input().split())
-# 벽 6번으로 가장 자리 막음
+
 arr = [[6] * (M+2)] + [[6] + list(map(int, input().split())) + [6] for _ in range(N)] + [[6] * (M+2)]
 
 lst = [] # cctv 좌표 저장
@@ -48,7 +48,7 @@ for i in range(1, N+1):
         if 1<=arr[i][j]<=5:
             lst.append((i, j))
 
-CNT = len(lst)
-ans = N*M
-dfs(0, [])
+CNT = len(lst) # cctv의 개수
+ans = N*M # 사각지대 넓이
+dfs(0, []) # 0번 cctv부터 []: cctv 회전 리스트
 print(ans)
