43c3fd9a26d265e7e8033fecec5a31c02fc26427
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2025-04-07 18:58:16 +0900

    [Gold IV] Title: 인구 이동, Time: 572 ms, Memory: 116568 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -32,11 +32,29 @@ while ans<=2000:
     # [1] 전체를 순회하면서, 미방문->연합 처리
     v = [[0] * N for _ in range(N)]
     flag = 0
+    q = deque()
     for i in range(N):
         for j in range(N):
             if v[i][j] == 0: # 미방문
-                flag = max(flag, bfs(i,j))
+                q.append((i, j))  # 큐에 초기데이터 삽입
+                v[i][j] = 1  # 방문 표시
+                alst = [(i, j)]  # 연합에 추가
+                sum = arr[i][j]  # 합계
 
+                while q:
+                    ci, cj = q.popleft()
+                    # 네 방향 범위내 미중복, 조건에 맞으면
+                    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                        ni, nj = ci + di, cj + dj
+                        if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0 and L <= abs(arr[ci][cj] - arr[ni][nj]) <= R:
+                            q.append((ni, nj))
+                            v[ni][nj] = 1
+                            alst.append((ni, nj))
+                            sum += arr[ni][nj]
+                if len(alst) > 1:  # 연합인 경우 (평균값 각각 저장)
+                    for ti, tj in alst:
+                        arr[ti][tj] = sum // len(alst)
+                    flag = 1
     if flag==0:
         break
     ans+=1
