720306f2be4cd5d7dfb3e8a28cf346a107041d44
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2025-04-23 19:03:45 +0900

    [Bronze I] Title: 디지털 티비, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2816. 디지털 티비/디지털 티비.py
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.py
@@ -9,21 +9,20 @@ while True:
     if arr[0] =='KBS1': # KBS1 완료 -> KBS2 1번으로 옮기기
         if arr[point] != 'KBS2':
             point += 1
-            ans.append(1)
+            ans.append("1")
         if arr[point] == 'KBS2':
             arr[point], arr[point-1] = arr[point-1], arr[point]
             point -= 1
-            ans.append(4)
+            ans.append("4")
 
     else: # KBS1을 0번으로 옮기기
         if arr[point] != 'KBS1':
             point += 1
-            ans.append(1)
+            ans.append("1")
         if arr[point] == 'KBS1':
             arr[point], arr[point-1] = arr[point-1], arr[point]
             point -= 1
-            ans.append(4)
+            ans.append("4")
             
 
-for i in range(len(ans)):
-    print(ans[i], end='')
+print(''.join(ans))
