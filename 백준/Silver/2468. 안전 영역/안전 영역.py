8df3f769f0f75c43b5ed2cef9cd31b5dc9d5030f
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2026-03-22 23:04:37 +0900

    [Silver I] Title: 안전 영역, Time: 228 ms, Memory: 116048 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,39 +1,40 @@
+# 영역의 개수: 네 방향, 범위 내, 미 방문, 특정 높이 이상일 때 안전 영역
+# 0~99까지 비가 온 높이별로 영역개수 구해서 최대값 구하면 됨
+
 from collections import deque
+def bfs(si, sj, h):
+    q = deque()
+    q.append((si, sj))
+    v[si][sj]=1
+
+    while q:
+        ci, cj = q.popleft()
+        # 네 방향, 범위내, 미방문, 높이보다 높은 경우
+        for di, dj in ((-1, 0), (1,0), (0,-1), (0,1)):
+            ni, nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<N and v[ni][nj]==0 and arr[ni][nj]>h:
+                q.append((ni, nj))
+                v[ni][nj]=1
+
+
+def solve(h): # h 높이에 대해 잠기지 않는 영역 개수 리턴
+    cnt = 0
+    for i in range(N):
+        for j in range(N):
+            if v[i][j]==0 and arr[i][j]>h:
+                bfs(i, j, h)
+                cnt += 1
+    
+    return cnt
+
 
 N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-area = [list(map(int, input().split())) for _ in range(N)]
-max_height = max(map(max, area))
-max_safe_areas = 0
+ans = 0
+for h in range(100): # 높이 0~99 까지 물 높이 지정
+    v = [[0]* N for _ in range(N)]
+    ans = max(ans, solve(h))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y, h, visited):
-    queue = deque([(x, y)])
-    visited[x][y] = True
-
-    while queue:
-        cx, cy = queue.popleft()
-
-        for k in range(4):
-            nx, ny = cx + dx[k], cy + dy[k]
-
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and area[nx][ny] > h:
-                queue.append((nx, ny))
-                visited[nx][ny] = True
-
-for h in range(0, max_height + 1):
-    visited = [[False] * N for _ in range(N)]
-    safe_area_count = 0
-
-    for i in range(N):
-        for j in range(N): 
-            if not visited[i][j] and area[i][j] > h:
-                bfs(i, j, h, visited)
-                safe_area_count += 1
-
-    max_safe_areas = max(max_safe_areas, safe_area_count)
-
-print(max_safe_areas)
+print(ans)
 
